3a804f2ad4a464d86b6e396d3904011e3498b19e
hujiali <375748201@qq.com>
2017-12-01 16:43:35 +0800

    reverse_进度条的添加（进行用户信息校验）

--- public/reverse/js/common.js
+++ public/reverse/js/common.js
@@ -1,29 +1,56 @@
-$(function(){
+$(function () {
+  // 去除圆弧进度条
+  NProgress.configure({
+    showSpinner:false
+  });
+  // 设置进度条
+  $(document).ajaxStart(function(){
+    NProgress.start();
+  });
+  $(document).ajaxStop(function(){
+    setTimeout(function () {
+      NProgress.done();
+    }, 500);
+  });
+
+  // 进行用户信息的判断
+  if( location.href.indexOf('login.html') == -1 ){
+    $.ajax({
+      type:'get',
+      url:'/employee/checkRootLogin',
+      success:function(data){
+        if(data.error == 404){
+          location.href = 'login.html';
+        }
+      }
+    });
+  }
+
   // 侧边栏分类管理动画
-  $('.categories').on('click',function(){
+  $('.categories').on('click', function () {
     $('.category').slideToggle();
   });
   // 头部动画
   var icon_menu = $('.lt_main .icon_menu');
   var icon_logout = $('.lt_main .icon_logout');
-  icon_menu.on('click',function(){
+  icon_menu.on('click', function () {
     $('.lt_aside').toggleClass('now');
     $('.lt_main').toggleClass('now');
   });
 
   /*  退出登录模态框 */
-  icon_logout.on('click',function(){
+  icon_logout.on('click', function () {
     $('.modal').modal('show');
   });
-  $('.btn_logout').on('click',function(){
+  $('.btn_logout').on('click', function () {
     // 隐藏模态框
     $('.modal').modal('hide');
     // 发送ajax请求
     $.ajax({
-      type:'get',
-      url:'/employee/employeeLogout',
-      success:function(data){
-        if( data.success ){
+      type: 'get',
+      url: '/employee/employeeLogout',
+      success: function (data) {
+        if (data.success) {
           location.href = 'login.html';
         }
       }
